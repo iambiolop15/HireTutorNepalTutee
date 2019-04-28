3d67818e85ad7a192779784a0431fdf273f6f601
iambiolop15 <biplopdangol123@gmail.com>
2019-04-28 21:29:36 +0545

    database

--- app/src/main/java/com/example/hiretutornepaltutee/ProfileFragment.java
+++ app/src/main/java/com/example/hiretutornepaltutee/ProfileFragment.java
@@ -8,20 +8,32 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class ProfileFragment extends Fragment {
     private FirebaseAuth firebaseAuth;
     RelativeLayout editProfile,myCourses,faq,sendFeedback,about,privacy,logout;
     Fragment fragment=null;
+    private FirebaseDatabase firebaseDatabase;
+    private TextView userId;
+    private ImageView profilePic;
 
 
 
@@ -31,7 +43,7 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_profile,null);
         firebaseAuth=FirebaseAuth.getInstance();
-
+        firebaseDatabase=FirebaseDatabase.getInstance();
         editProfile=view.findViewById(R.id.editprofileLayout);
         myCourses=view.findViewById(R.id.CoursesLayout);
         faq=view.findViewById(R.id.FAQLayout);
@@ -39,6 +51,24 @@ public class ProfileFragment extends Fragment {
         about=view.findViewById(R.id.AboutLayout);
         privacy=view.findViewById(R.id.PrivacyLayout);
         logout=view.findViewById(R.id.LogoutLayout);
+        profilePic=view.findViewById(R.id.profilepic);
+        userId=view.findViewById(R.id.Username);
+        DatabaseReference databaseReference=firebaseDatabase.getReference(firebaseAuth.getUid());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserProfile userProfile=dataSnapshot.getValue(UserProfile.class);
+                userId.setText(userProfile.getUname());
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getActivity(), databaseError.getCode(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/hiretutornepaltutee/UserProfile.java
+++ app/src/main/java/com/example/hiretutornepaltutee/UserProfile.java
@@ -8,7 +8,7 @@ public class UserProfile {
 
     public String ugender;
 
-    public UserProfile() {
+    public  UserProfile() {
 
     }
 
@@ -18,5 +18,45 @@ public class UserProfile {
         this.uage = age;
         this.uphone = phone;
         ugender = gender;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getUemail() {
+        return uemail;
+    }
+
+    public void setUemail(String uemail) {
+        this.uemail = uemail;
+    }
+
+    public String getUage() {
+        return uage;
+    }
+
+    public void setUage(String uage) {
+        this.uage = uage;
+    }
+
+    public String getUphone() {
+        return uphone;
+    }
+
+    public void setUphone(String uphone) {
+        this.uphone = uphone;
+    }
+
+    public String getUgender() {
+        return ugender;
+    }
+
+    public void setUgender(String ugender) {
+        this.ugender = ugender;
     }
 }
